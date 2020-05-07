594f596a218068a56162131e80d5c8a772c44554
Scott Trinkle <tscott.trinkle@gmail.com>
2020-05-07 11:07:19 -0500

    Adds .nii.gz to .tif converter for dec images

--- mrpy/utils.py
+++ mrpy/utils.py
@@ -1,4 +1,6 @@
 import nibabel as nib
+import numpy as np
+from skimage.io import imsave
 
 
 def loadnii(fn):
@@ -8,3 +10,12 @@ def loadnii(fn):
 
 def savenii(data, aff, fn):
     nib.save(nib.Nifti1Image(data, aff), fn)
+
+
+def dec2tif(fn):
+    img, data = loadnii(fn)
+    data = np.moveaxis(data, [0, 1, 2, 3], [2, 0, 1, 3])
+    data = np.flip(data, axis=1)
+    data = (255 * (data - data.min()) /
+            (data - data.min()).max()).astype(np.uint8)
+    imsave(fn.split('.nii')[0] + '.tif', data)
